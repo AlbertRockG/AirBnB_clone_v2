25de4f6a1d7007b93cd5798116050294ead095e5
bioaddict <rafgangbadja@gmail.com>
2023-10-09 01:41:14 +0100

    Improve nginx configuration

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """Fabfile to distribute an archive to a web server."""
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run, cd
 
+
+env.user = "ubuntu"
+env.hosts =  ["54.209.135.247", "3.90.85.92"]
+env.key_filename = "~/.ssh/known_hosts"
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
     Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
-        False, if the file doen't exist at the given archive_path,
+        False, if the file doesn't exist at the given archive_path,
         True, otherwise.
     """
     if os.path.isfile(archive_path) is False:
@@ -19,27 +21,31 @@ def do_deploy(archive_path):
     myfile = archive_path.split("/")[-1]
     name = myfile.split(".")[0]
 
-    with settings(hosts = ['ubuntu@54.209.135.247', 'ubuntu@3.90.85.92'], key_filename = "~/.ssh/id_rsa"):
-    
-        if put(archive_path, "/tmp/{}/\
-            ".format(myfile)).failed is True:
+    print("New version deployment started at!")
+    with cd("/tmp"):
+        if put(local_path=archive_path,
+                remote_path="{}".format(myfile),
+                mirror_local_mode=True, use_sudo=True).failed is True:
             return False
-
-        if run("mkdir -p /data/web_static/releases/{}/\
-            ".format(name)).failed is True:
+    if run("ls -l /tmp/").failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/\
+        ".format(name)).failed is True:
+        return False
+    if run("tar -xvzf /tmp/{} -C /data/web_static/releases/{}\
+        ".format(myfile, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(myfile)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run(
+        "rm -rf /data/web_static/releases/{}/web_static/".format(name)
+        ).failed is True:
+        return False
+    if run("ln -sfn /data/web_static/releases/{}/\
+        /data/web_static/current".format(name)).failed is True:
             return False
-        if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/\
-            ".format(myfile, name)).failed is True:
-            return False
-        if run("rm /tmp/{}".format(myfile)).failed is True:
-            return False
-        if run("mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/".format(name, name)).failed is True:
-            return False
-        if run("rm -rf /data/web_static/releases/{}/\
-            web_static".format(name)).failed is True:
-            return False
-        if run("ln -sf /data/web_static/releases/{}/\
-            /data/web_static/current".format(name)).failed is True:
-            return False
+    print("New version deployed!")
     return True
