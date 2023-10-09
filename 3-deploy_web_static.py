ef819d4b88e37f86b5bbcf0483de5090ab78374d
bioaddict <rafgangbadja@gmail.com>
2023-10-09 01:46:06 +0100

    Add env variables

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,9 @@ from datetime import datetime
 from fabric.api import *
 
 
-env.hosts = ['ubuntu@54.209.135.247', 'ubuntu@3.90.85.92']
+env.user = "ubuntu"
+env.hosts =  ["54.209.135.247", "3.90.85.92"]
+env.key_filename = "~/.ssh/known_hosts"
 
 def do_pack():
     """Creates a tar gzipped archive of the directory web_static"""
@@ -29,7 +31,7 @@ def do_deploy(archive_path):
     Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
-        False, if the file doen't exist at the given archive_path,
+        False, if the file doesn't exist at the given archive_path,
         True, otherwise.
     """
     if os.path.isfile(archive_path) is False:
@@ -37,28 +39,35 @@ def do_deploy(archive_path):
     myfile = archive_path.split("/")[-1]
     name = myfile.split(".")[0]
 
-    if put(archive_path, "/tmp/{}/\
-           ".format(myfile)).failed is True:
+    print("New version deployment started at!")
+    with cd("/tmp"):
+        if put(local_path=archive_path,
+                remote_path="{}".format(myfile),
+                mirror_local_mode=True, use_sudo=True).failed is True:
+            return False
+    if run("ls -l /tmp/").failed is True:
         return False
-    
     if run("mkdir -p /data/web_static/releases/{}/\
-           ".format(name)).failed is True:
+        ".format(name)).failed is True:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/\
-           ".format(myfile, name)).failed is True:
+    if run("tar -xvzf /tmp/{} -C /data/web_static/releases/{}\
+        ".format(myfile, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(myfile)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/*\
         /data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/\
-        web_static".format(name)).failed is True:
+    if run(
+        "rm -rf /data/web_static/releases/{}/web_static/".format(name)
+        ).failed is True:
         return False
-    if run("ln -sf /data/web_static/releases/{}/\
+    if run("ln -sfn /data/web_static/releases/{}/\
         /data/web_static/current".format(name)).failed is True:
-        return False
+            return False
+    print("New version deployed!")
     return True
+
 
 def deploy():
     """Creates and distributes an archive to web servers.
