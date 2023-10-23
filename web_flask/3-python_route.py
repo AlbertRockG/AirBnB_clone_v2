f27f174a882225032c450c07dce62e059e42718d
bioaddict <rafgangbadja@gmail.com>
2023-10-23 03:57:18 +0100

    Fix styling

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -34,6 +34,7 @@ def display_c(text):
     """
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def display_python(text='is cool'):
